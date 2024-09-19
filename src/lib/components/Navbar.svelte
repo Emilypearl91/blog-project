b1917f0d2f80bacadc5aa0ed3b36aff246e83883
Emily Pearl <Emilypearl9193@gmail.com>
2024-09-18 18:31:45 -0600

    added navbar

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,28 @@
+<nav class="bg-[#211B27] h-16">
+<ul>
+  <li><a href="/">Home</a></li>
+ <li><a href="/announcements">Announcements</a></li>
+ <li><a href="/posts">Posts</a></li>
+ <li><a href="/about-me">About Me</a></li>
+</ul>
+</nav>
+
+<style>
+  
+   ul {
+    display: flex;
+    flex-direction: row;
+    justify-content: right;
+   }
+
+   li {
+    margin: .5rem;
+
+    list-style-type: none;
+   }
+
+   a {
+    text-decoration: none;
+    color: #BBA786;
+   }
+</style>
