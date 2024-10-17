ed124cf90b0f0c85cff1c07482a6992250c731a1
Emily Pearl <Emilypearl9193@gmail.com>
2024-10-16 21:31:23 -0600

    added accordion with data to posts page

---
+++ src/routes/posts/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+
+import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
+ export let data;
+</script>
+<Accordion autocollapse class="max-w-2xl bg-[#211B27] text-[#BBA786] m-auto rounded-md">
+  {#each data.blog as post}
+  <AccordionItem open>
+    <svelte:fragment slot="lead">{post.title}</svelte:fragment>
+    <svelte:fragment slot="summary">{post.date}</svelte:fragment>
+    <svelte:fragment slot="content">{post.body}</svelte:fragment>
+  </AccordionItem>
+  {/each} 
+</Accordion>
