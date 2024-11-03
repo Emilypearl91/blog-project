c1320cd46d256a05638544c66f3096a3262dbe17
Emily Pearl <Emilypearl9193@gmail.com>
2024-11-03 10:00:36 -0700

    set up google analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,10 @@
 <script>
   import "../app.css";
+	import Analytics from "../lib/analytics.svelte";
 	import Navbar from "../lib/components/Navbar.svelte";
 </script>
 <Navbar/>
   
-
+<Analytics/>
 <slot/>
 
